6a2350087df92a9bca28db3b11ed69f01e6ca4c4
OnionchipBox <10823syn@naver.com>
2023-09-22 16:00:34 +0900

    시큐리티 중간 적용

---
+++ src/main/java/com/team3web/shop/service/UserServiceImpl.java
@@ -0,0 +1,84 @@
+package com.team3web.shop.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+<<<<<<< HEAD
+<<<<<<< HEAD
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+=======
+import org.springframework.security.crypto.password.PasswordEncoder;
+>>>>>>> 6a3530dc46bcab8f96a465db2171416df037c414
+=======
+import org.springframework.security.crypto.password.PasswordEncoder;
+>>>>>>> 6a3530dc46bcab8f96a465db2171416df037c414
+import org.springframework.stereotype.Service;
+import com.team3web.shop.dao.UserDAO;
+import com.team3web.shop.vo.UserVO;
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+	private final UserDAO userDAO;
+<<<<<<< HEAD
+<<<<<<< HEAD
+	
+	@Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserDAO userDAO) {
+        this.userDAO = userDAO;
+=======
+=======
+>>>>>>> 6a3530dc46bcab8f96a465db2171416df037c414
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImpl(UserDAO userDAO, PasswordEncoder passwordEncoder) {
+        this.userDAO = userDAO;
+        this.passwordEncoder = passwordEncoder;
+<<<<<<< HEAD
+>>>>>>> 6a3530dc46bcab8f96a465db2171416df037c414
+=======
+>>>>>>> 6a3530dc46bcab8f96a465db2171416df037c414
+    }
+
+    @Override
+    public void insertUser(UserVO user) {
+    	user.setPassword(passwordEncoder.encode(user.getPassword()));	// 가입시 비밀번호 암호화
+        userDAO.insertUser(user);
+    }
+
+    @Override
+    public void updateUser(UserVO user) {
+        userDAO.updateUser(user);
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+        userDAO.deleteUser(userId);
+    }
+
+    @Override
+    public UserVO getUserById(String userId) {
+        return userDAO.getUserById(userId);
+    }
+
+    @Override
+    public List<UserVO> getAllUsers() {
+        return userDAO.getAllUsers();
+    }
+	
+	@Override
+    public int getUserRole(int verify) {
+        return userDAO.getUserRole(verify);
+<<<<<<< HEAD
+<<<<<<< HEAD
+        
+=======
+>>>>>>> 6a3530dc46bcab8f96a465db2171416df037c414
+=======
+>>>>>>> 6a3530dc46bcab8f96a465db2171416df037c414
+    }
+}
