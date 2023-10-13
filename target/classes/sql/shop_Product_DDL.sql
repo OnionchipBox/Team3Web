8968967f8137aebbb6bd65d361a53e7a7af46e73
jihyeon <wusl3954@gmail.com>
2023-10-13 21:39:52 +0900

    헤더수정중

--- target/classes/sql/shop_Product_DDL.sql
+++ target/classes/sql/shop_Product_DDL.sql
@@ -7,7 +7,7 @@ CREATE TABLE shop_products (
     pcode VARCHAR2(255) NOT NULL,
     thumbimg varchar2(255) not null,
     category varchar2(255) ,
-     seller_id number(38)       --ì¶”í›„ seller idê°? ì°¸ì¡°(FK) ì¶”ê?(?–´?–¤ ?Œë§¤ìžê°? ?˜¬ë¦? ?ƒ?’ˆ?¸ì§? ?™•?¸?•  ?ˆ˜ ?žˆ?–´?•¼ ?•¨)
+     seller_id number(38)       --ì¶”í›„ seller idï¿½? ì°¸ì¡°(FK) ì¶”ï¿½?(?ï¿½ï¿½?ï¿½ï¿½ ?ï¿½ï¿½ë§¤ìžï¿½? ?ï¿½ï¿½ï¿½? ?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½ï¿½? ?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½ ?ï¿½ï¿½ ?ï¿½ï¿½?ï¿½ï¿½?ï¿½ï¿½ ?ï¿½ï¿½)
 );
 
 create SEQUENCE product_no_seq
@@ -15,3 +15,30 @@ start with 1
 increment by 1
 nocache
 nocycle;
+
+select * from shop_products;
+
+commit;
+
+create table shop_user
+(
+
+  ID        VARCHAR2(50)         PRIMARY KEY,
+  PASSWORD        VARCHAR2(100)        ,
+  NAME      VARCHAR2(30)                  ,
+  GENDER      VARCHAR2(10)                  ,
+  PHONE     VARCHAR2(20)                ,
+  NICKNAME VARCHAR2(30)                ,
+  ZIPCODE VARCHAR2(10)   ,
+  ADDRESS   VARCHAR2(400)                  ,
+  BIRTHDAY  DATE                  ,
+  JOINDATE  VARCHAR2(20)                  ,
+  UPDATEDATE VARCHAR2(20)   ,
+    VERIFY    VARCHAR2(30) DEFAULT 'USER' CHECK (VERIFY IN ('USER', 'SELLER', 'ADMIN')) ,
+    BALANCE NUMBER(10, 0) DEFAULT 0 ,
+    POINT NUMBER(10, 0) DEFAULT 1000
+);
+
+
+select * from shop_user;
+
