3f72441dc533b1838f9bcdb0952fbe5257ef2fe8
OnionchipBox <10823syn@naver.com>
2023-10-24 17:12:45 +0900

    이동

---
+++ src/main/java/com/team3web/shop/service/LoginService.java
@@ -0,0 +1,28 @@
+package com.team3web.shop.service;
+
+import javax.servlet.http.HttpSession;
+
+import com.team3web.shop.vo.UserVO;
+
+public interface LoginService {
+	
+	public int loginCheck(UserVO user, HttpSession session);
+	
+	public UserVO viewUser(UserVO user);
+	
+	public void logout(HttpSession session);
+	
+	public String idFind(UserVO user);
+	
+	public String pwFind(UserVO user);
+
+	public void updatePw(UserVO user);
+	
+	public String getPasswordById(String id);
+	
+	public UserVO getUserById(String id);
+	
+	public int getUserRole(String id);
+	
+	public String getUserName(String id);
+}
